9bf3c2327e73315920fd2aca8079d8c8b64c7c5b
Coriano Harris <corianoharris@gmail.com>
2023-05-21 19:56:30 -0700

    Added renderListItems fn

--- main.js
+++ main.js
@@ -1,7 +1,71 @@
+window.addEventListener("load", (event) => {
+  renderListItems(COMMUNITIES, COMMUNITY_LIST);
+  renderListItems(CLIENTS, CLIENTS_LIST);
+  renderListItems(GRAPHIC_DESIGN, GRAPHIC_LIST);
+  renderListItems(UX, UX_LIST);
+  renderListItems(UI, UI_LIST);
+  renderListItems(DEV, DEV_LIST);
+});
+
+/**
+ * CONSTANTS
+ * LISTS
+ */
+
+const CLIENTS = [
+  "Partcycle",
+  "CodeCrew",
+  "Dare Dream Dance",
+  "C4 Atlanta",
+  "Lauren Brynes Productions",
+  "St. John’s Confidential File",
+  "Juice Mound Memphis",
+  "Navy Seals Foundation Memphis",
+  "Sweet Cheeks Ministry",
+];
+const COMMUNITIES = ["Tech901", "GiveCamp Memphis", "UX Mastery"];
+const GRAPHIC_DESIGN = ["page layout"];
+const UX = ["quantitative research", "analytics"];
+const UI = ["sketch", "invision", "mockups", "color theory", "adobe xd"];
+const DEV = [
+  "sass",
+  "ember.js",
+  "next.js",
+  "node.js",
+  "docusaurus",
+  "airtable",
+  "restful api",
+  "netlify",
+  "AXE a11y scan",
+  "pair programming",
+  "cross browser testing",
+];
+// const SOFT_SKILLS = [""];
+
+// ul list elements
+const CLIENTS_LIST = document.getElementById("js-clients-list");
+const COMMUNITY_LIST = document.getElementById("js-community-list");
+const GRAPHIC_LIST = document.getElementById("js-graphic-list");
+const UX_LIST = document.getElementById("js-ux-list");
+const UI_LIST = document.getElementById("js-ui-list");
+const DEV_LIST = document.getElementById("js-dev-list");
+const SOFT_LIST = document.getElementById("js-soft-list");
+
+// util functions
+
+function renderListItems(array, element) {
+  for (item of array) {
+    let li = document.createElement("li");
+    li.innerHTML = item;
+    li.classList.add("list-items");
+    element.appendChild(li);
+  }
+}
+
+// navigation
 const navLinks = document.querySelectorAll(".nav-link-text");
 const firstNavLink = document.querySelectorAll(".nav-items")[0];
 const lastNavLink = document.querySelectorAll(".nav-items")[2];
-const titles = document.querySelectorAll(".title");
 
 firstNavLink.addEventListener("keydown", (e) => {
   if (e.shiftKey && e.key === "Tab") {
@@ -42,6 +106,7 @@ function closeNav() {
   document.getElementById("myNav").style.display = "none";
 }
 
+// form
 const form = document.forms["contact"];
 
 if (form) {
@@ -65,8 +130,6 @@ function contact_submit(e) {
   var email = document.getElementById("email").value;
   var tel = document.getElementById("phone").value;
   var message = document.getElementById("message").value;
-
-  console.log(name);
 
   // Build Body / Message with all Input Fields
   body += "Name: " + name + "\xa0" + "\r\n";
