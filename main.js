47c969bc30957544579ea464e76541b63347d667
Coriano Harris <corianoharris@Corianos-MBP.attlocal.net>
2024-06-06 23:15:47 -0700

    Update website experience

--- main.js
+++ main.js
@@ -284,17 +284,20 @@
     // Event listeners
     window.addEventListener("resize", check);
   
-  
- 
-  
     navBtn.addEventListener("click", function () {
       mainNavHiddenlinks.classList.toggle("hidden");
       tooltip.classList.toggle("hidden");
-    });
 
-   
+      const icon = this.querySelector('i');
+    if (icon.classList.contains('fa-bars')) {
+        icon.classList.remove('fa-bars');
+        icon.classList.add('fa-times');
+    } else {
+        icon.classList.remove('fa-times');
+        icon.classList.add('fa-bars');
+    }
+});
 
-    
 
   
     prevBtn.addEventListener("click", () => {
